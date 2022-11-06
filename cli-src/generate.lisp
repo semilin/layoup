b8c143e91bbe9907dd82618958495f9d786eded1
semilin <semilinares@pm.me>
2022-11-05 18:05:27 -0700

    small generation adjustments

--- cli-src/generate.lisp
+++ cli-src/generate.lisp
@@ -5,7 +5,7 @@
 	    :col (random 10)
 	    :row (random 3)))
 
-(defun constraint-deviance (stats constraints)
+(defun constraint-deviance (stats constraints max-threshold)
   (reduce #'+ (mapcar (lambda (c)
 			(if (funcall (ecase (constraint-goal c)
 				       (:less '<=)
@@ -13,8 +13,10 @@
 				     (gethash (constraint-metric c) stats)
 				     (constraint-threshold c))
 			    0
-			    (* 4 (abs (- (constraint-threshold c)
-					 (gethash (constraint-metric c) stats))))))
+			    (/ (* (/ max-threshold (constraint-threshold c))
+				  (abs (- (constraint-threshold c)
+					  (gethash (constraint-metric c) stats))))
+			       (* 2 (length constraints)))))
 		      constraints)))
 
 (defun sum-deviance-optimized (deviance opt-comparator optimized stats)
@@ -33,6 +35,8 @@
 						      :threshold (* (keys-total keys corpus)
 								    (/ (constraint-threshold c) 100))))
 			      (constraint-profile-constraints profile)))
+	 (max-threshold (apply #'max (mapcar (lambda (c) (constraint-threshold c))
+					     constraints)))
 	 (metric-results (layoup:calculate-metrics
 			  (defaults-keyboard *defaults*)
 			  ;; reduces *metrics* to only the metrics needed
@@ -51,21 +55,19 @@
 										  constraints)))
 							(metric-list-trigraphs *metrics*)))))
 	 (current-stats (layoup:analyze-keys corpus keys metric-results))
-	 (current-deviance (constraint-deviance current-stats constraints))
+	 (current-deviance (constraint-deviance current-stats constraints max-threshold))
 	 (current-total (sum-deviance-optimized current-deviance opt-comparator optimized current-stats)))
-    (format t "~a~%" opt-comparator)
-    (format t "~{~a~%~}~%" constraints)
-    (loop for temp downfrom 100 to -4
+    (loop for temp downfrom 100 to -10
 	  do (progn
 	       (format t "~C~a%    " #\return temp)
 	       ;;(format t "~a%: ~a + ~a = ~a~%" temp current-deviance (gethash optimized current-stats) current-total)
 	       (finish-output)
-	       (dotimes (i (* 5 (- 100 temp)))
+	       (dotimes (i (* 10 (- 100 temp)))
 		 (let ((a (random-pos))
 		       (b (random-pos)))
 		   (swap-keys a b keys)
 		   (let* ((new-stats (layoup:analyze-keys corpus keys metric-results))
-			  (new-deviance (constraint-deviance new-stats constraints))
+			  (new-deviance (constraint-deviance new-stats constraints max-threshold))
 			  (new-total (sum-deviance-optimized new-deviance opt-comparator optimized new-stats)))
 		     (if (<= new-total current-total)
 			 (setf current-stats new-stats
